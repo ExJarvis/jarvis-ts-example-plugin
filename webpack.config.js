8700c8ed45608c3572ab036e4cd865d4f3441755
Pulkit Singh <pulkit.singh@shipsy.co.in>
2020-11-26 16:59:40 +0530

    webpack bundling

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const {
+  NODE_ENV = 'production',
+} = process.env;
+module.exports = {
+  entry: './src/main.ts',
+  mode: NODE_ENV,
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          'ts-loader',
+        ]
+      }
+    ]
+  }
+}
